f49fbce60c275717a6e70f780c0df3748e8fb19c
manjunadhreddy <49943566+manjunadhreddy@users.noreply.github.com>
2019-04-26 21:10:02 +0530

    Further Calculations

--- main.c
+++ main.c
@@ -24,5 +24,22 @@ int main()  {
             rt[count]-=time_quantum;
             time+=time_quantum;
         }
+        
+        if(rt[count]==0 && flag==1)      {
+            remain--;
+            printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]);
+            wait_time+=time-at[count]-bt[count];
+            turnaround_time+=time-at[count];
+            flag=0;
+        }
+        if(count==n-1){
+            count=0;
+        }
+        else if(at[count+1]<=time){
+            count++;
+        }
+        else{
+            count=0;
+        }
     }
 }
