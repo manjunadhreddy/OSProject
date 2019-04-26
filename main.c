3f7553e1fe63ac7215142836916be57d4a4d7fa1
manjunadhreddy <49943566+manjunadhreddy@users.noreply.github.com>
2019-04-26 21:06:47 +0530

    Initial

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()  {
+    int count,j,n,time,remain,flag=0,time_quantum;
+    int wait_time=0,turnaround_time=0,at[10],bt[10],rt[10];
+    printf("Enter Total Process:\t ");
+    scanf("%d",&n);
+    remain=n;
+}
