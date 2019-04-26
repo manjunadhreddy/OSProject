9301fab84d9ffe365f6571c1f6881aae31afef16
manjunadhreddy <49943566+manjunadhreddy@users.noreply.github.com>
2019-04-26 21:09:11 +0530

    Time Quantum

--- main.c
+++ main.c
@@ -13,4 +13,16 @@ int main()  {
         scanf("%d",&bt[count]);
         rt[count]=bt[count];
     }
+    
+    for(time=0,count=0;remain!=0;)    {
+        if(rt[count]<=time_quantum && rt[count]>0)      {
+            time+=rt[count];
+            rt[count]=0;
+            flag=1;
+        }
+        else if(rt[count]>0)      {
+            rt[count]-=time_quantum;
+            time+=time_quantum;
+        }
+    }
 }
